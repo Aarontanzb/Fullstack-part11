190c16524ce2da9ad80dd8835fb30a2d5367ccd4
Aaron Tan <aarontanzb@gmail.com>
2023-03-11 14:45:56 +0800

    Initial commit

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const cors = require('cors')
+
+const app = express()
+
+// Heroku dynamically sets a port
+const PORT = process.env.PORT || 5000
+
+app.use(express.static('dist'))
+app.use(cors())
+
+app.listen(PORT, () => {
+  // eslint-disable-next-line no-console
+  console.log('server started on port 5000')
+})
---
+++ exercise1.md
@@ -0,0 +1,6 @@
+Language: JS
+Linting: eslint
+Testing: Jest, cypress
+Building: webpack
+Alternatives: Azure devops, circleci, gitlab ci, bitbucket pipelines
+Self-hosted vs cloud-based: cloud better due to us being a small/medium sized application, less overhead setup and config, cheaper, no complex systems to create
